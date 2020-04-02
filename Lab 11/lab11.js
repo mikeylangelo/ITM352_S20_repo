e077517e62c5aee7434dcece4c7bb7d2fb9586c4
mikeylangelo <60157511+mikeylangelo@users.noreply.github.com>
2020-04-02 11:00:15 -1000

    4/2

---
+++ Lab 11/lab11.js
@@ -0,0 +1,8 @@
+function isNonNegInt(q){
+    errors = []; // assume no errors at first
+    if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+    return pieces
+    console.log( (typeof item == 'string' && item.length > 0)?true:false )
+}
