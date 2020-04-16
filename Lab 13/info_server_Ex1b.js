aa8a7370286d7643f5137269c1e7a6d1f00229f5
mikeylangelo <60157511+mikeylangelo@users.noreply.github.com>
2020-04-16 12:15:10 -1000

    4/16

--- Lab 13/info_server_Ex1b.js
+++ Lab 13/info_server_Ex1b.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 app.all('*', function (request, response, next) {
-    response.send(request.method + ' to path ' + request.path);
+    console.log(request.method + ' to ' + request.path);
+    next();
 });
+app.use(express.static('./public'));
 app.listen(8080, () => console.log(`listening on port 8080`));
