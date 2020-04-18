b8de0108a75f65f9b0c9d4eaa5a4053c6f926d4e
mikeylangelo <60157511+mikeylangelo@users.noreply.github.com>
2020-04-17 21:00:06 -1000

    4/17

--- Ferrer_Michelangelo_Assignment1/server.js
+++ Ferrer_Michelangelo_Assignment1/server.js
@@ -4,61 +4,96 @@ Based on Assignment 1 examples.
 
 var express = require('express'); //Load express for handling HTTP requests.
 
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); //Load bodyParser for parsing any data that passes through the server.
 
-var fs = require('fs'); //
+var fs = require('fs'); //Load file system.
 
 var app = express();
 
-var data = require('./public/product_data.js');
+var data = require('./public/product_data.js'); 
 
-var products = data.products;
+var products = data.products; //Call in data from public directory and assign it to a variable.
 
-var queryString = require("querystring");
+app.use(bodyParser()); 
 
-app.use(bodyParser());
-
+//Indicate any requests coming in from the server
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path);
     next();
 });
 
+    /* 
+    The structure was based on the MVC Assignment 1 example.
+    We use the server to host the data and ask the public directory for the files
+    such as the display.html and invoice.html which contains the layout for the functions
+    which inputs the data the server has on to the static files.
+    */
+
+
+app.get("/display.html", function (request, response) {
+    var contents = fs.readFileSync('./public/display.html', 'utf8');
+    response.send(eval('`' + contents + '`')); // render template string
+
+    function display_products() {
+        str = '';
+        for (i = 0; i < products.length; i++) {
+            str += `
+            <section>
+            <div>
+            <img src="/images/${products[i].image}">
+            </div>
+         </section>
+         <section>
+            <div>
+             ${products[i].name}:
+             $${products[i].price.toFixed(2)}
+            
+             <input type="text" placeholder="0" name="${products[i].name}" 
+             onkeyup="checkQuantityTextbox(this);">
+             </div
+          
+         </section>
+            `;
+        }
+        return str;
+    }
+});
+
 app.post("/display.html", function (request, response, ) {
     let POST = request.body;
-    if (typeof POST['purchase_flowers'] == 'undefined') {
-        console.log('No purchase');
-        next();
+    p=POST["purchase_flowers"]
+    if (isNonNegInt(p,false)){
+        response.send(`Help`);
     }
-
-    console.log(Date.now() + ': Purchase made from ip ' + request.ip + ' data: ' + JSON.stringify(POST));
-
+else{
     var contents = fs.readFileSync('./public/invoice.html', 'utf8');
     response.send(eval('`' + contents + '`'));
 
+    //Used in response.send(eval(contents))
     function display_invoice_table_rows() {
         subtotal = 0;
         str = '';
         for (i = 0; i < products.length; i++) {
-            a_qty = 0;
+            p = 0;
             if (typeof POST[`${products[i].name}`] != 'undefined') {
-                a_qty = POST[`${products[i].name}`];
+                p = POST[`${products[i].name}`];
             }
-            if (a_qty > 0) {
+            if (p > 0) {
                
-                extended_price = a_qty * products[i].price
+                extended_price = p * products[i].price
                 subtotal += extended_price;
                 str += (`
       <tr>
         <td width="43%">${products[i].name}</td>
-        <td align="center" width="11%">${a_qty}</td>
-        <td width="13%">\$${products[i].price}</td>
-        <td width="54%">\$${extended_price}</td>
+        <td align="center" width="11%">${p}</td>
+        <td width="13%">\$${products[i].price.toFixed(2)}</td>
+        <td width="54%">\$${extended_price.toFixed(2)}</td>
       </tr>
       `);
             }
         }
         
-        /* Based on Order Page of Lab 12*/
+        /* Based on order_page.html of Lab 12*/
         
         // Compute tax
         tax_rate = 0.0575;
@@ -76,13 +111,29 @@ app.post("/display.html", function (request, response, ) {
         }
 
         // Compute grand total
-        total = subtotal + tax + shipping;
+        grandtotal = subtotal + tax + shipping;
 
         return str;
     }
 
-});
+}});
 
 app.use(express.static('./public')); //Hosts static files in public directory.
 
-app.listen(8080, () => console.log(`Listening...`));
+app.listen(8080, () => console.log(`Listening...`)); //Location of port and send a message to console to indicate running server.
+
+function isNonNegInt(p, return_errors = false) {
+    errors = []; // assume no errors at first
+    if(p == '') p =0; // handle blank inputs as if they are 0
+    if (Number(p) != p) errors.push('<font color="red">Not a number!</font>'); // Check if string is a number value
+    else if (p < 0) errors.push('<font color="red">Negative value!</font>'); // Check if it is non-negative
+    else if (parseInt(p) != p) errors.push('<font color="red">Not an integer!</font>'); // Check that it is an integer
+    return return_errors ? errors : (errors.length == 0);
+}
+
+function checkQuantityTextbox(theTextbox) {
+    errs = isNonNegInt(theTextbox.value, true);
+    if (errs.length == 0) errs = ['You want:'];
+    if (theTextbox.value.trim() == '') errs = ['Quantity'];
+    document.getElementById(theTextbox.name + '_label').innerHTML = errs.join(", ");
+}
