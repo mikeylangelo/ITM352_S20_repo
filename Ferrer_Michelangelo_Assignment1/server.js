407063bd93e00e4576ba81b99ff2e24697e51bd0
mikeylangelo <60157511+mikeylangelo@users.noreply.github.com>
2020-04-17 23:54:21 -1000

    4/17

--- Ferrer_Michelangelo_Assignment1/server.js
+++ Ferrer_Michelangelo_Assignment1/server.js
@@ -10,11 +10,11 @@ var fs = require('fs'); //Load file system.
 
 var app = express();
 
-var data = require('./public/product_data.js'); 
+var data = require('./public/product_data.js');
 
 var products = data.products; //Call in data from public directory and assign it to a variable.
 
-app.use(bodyParser()); 
+app.use(bodyParser());
 
 //Indicate any requests coming in from the server
 app.all('*', function (request, response, next) {
@@ -22,12 +22,12 @@ app.all('*', function (request, response, next) {
     next();
 });
 
-    /* 
-    The structure was based on the MVC Assignment 1 example.
-    We use the server to host the data and ask the public directory for the files
-    such as the display.html and invoice.html which contains the layout for the functions
-    which inputs the data the server has on to the static files.
-    */
+/* 
+The structure was based on the MVC Assignment 1 example.
+We use the server to host the data and ask the public directory for the files
+such as the display.html and invoice.html which contains the layout for the functions
+which inputs the data the server has on to the static files.
+*/
 
 
 app.get("/display.html", function (request, response) {
@@ -61,28 +61,26 @@ app.get("/display.html", function (request, response) {
 
 app.post("/display.html", function (request, response, ) {
     let POST = request.body;
-    p=POST["purchase_flowers"]
-    if (isNonNegInt(p,false)){
-        response.send(`Help`);
-    }
-else{
-    var contents = fs.readFileSync('./public/invoice.html', 'utf8');
-    response.send(eval('`' + contents + '`'));
+    for (i = 0; i < products.length; i++) {
+        p = POST[`${products[i].name}`]
+        if (isNonNegInt(p, false)) {
+            var contents = fs.readFileSync('./public/invoice.html', 'utf8');
+            response.send(eval('`' + contents + '`'));
 
-    //Used in response.send(eval(contents))
-    function display_invoice_table_rows() {
-        subtotal = 0;
-        str = '';
-        for (i = 0; i < products.length; i++) {
-            p = 0;
-            if (typeof POST[`${products[i].name}`] != 'undefined') {
-                p = POST[`${products[i].name}`];
-            }
-            if (p > 0) {
-               
-                extended_price = p * products[i].price
-                subtotal += extended_price;
-                str += (`
+            //Used in response.send(eval(contents))
+            function display_invoice_table_rows() {
+                subtotal = 0;
+                str = '';
+                for (i = 0; i < products.length; i++) {
+                    p = 0;
+                    if (typeof POST[`${products[i].name}`] != 'undefined') {
+                        p = POST[`${products[i].name}`];
+                    }
+                    if (p > 0) {
+
+                        extended_price = p * products[i].price
+                        subtotal += extended_price;
+                        str += (`
       <tr>
         <td width="43%">${products[i].name}</td>
         <td align="center" width="11%">${p}</td>
@@ -90,33 +88,39 @@ else{
         <td width="54%">\$${extended_price.toFixed(2)}</td>
       </tr>
       `);
-            }
-        }
-        
-        /* Based on order_page.html of Lab 12*/
-        
-        // Compute tax
-        tax_rate = 0.0575;
-        tax = tax_rate * subtotal;
+                    }
+                }
 
-        // Compute shipping
-        if (subtotal <= 50) {
-            shipping = 2;
-        }
-        else if (subtotal <= 100) {
-            shipping = 5;
+                /* Based on order_page.html of Lab 12*/
+
+                // Compute tax
+                tax_rate = 0.0575;
+                tax = tax_rate * subtotal;
+
+                // Compute shipping
+                if (subtotal <= 50) {
+                    shipping = 2;
+                }
+                else if (subtotal <= 100) {
+                    shipping = 5;
+                }
+                else {
+                    shipping = 0.05 * subtotal; // 5% of subtotal
+                }
+
+                // Compute grand total
+                grandtotal = subtotal + tax + shipping;
+
+                return str;
+            }
+
         }
         else {
-            shipping = 0.05 * subtotal; // 5% of subtotal
+            response.redirect('errorpage.html');
+
         }
-
-        // Compute grand total
-        grandtotal = subtotal + tax + shipping;
-
-        return str;
     }
-
-}});
+});
 
 app.use(express.static('./public')); //Hosts static files in public directory.
 
@@ -124,10 +128,10 @@ app.listen(8080, () => console.log(`Listening...`)); //Location of port and send
 
 function isNonNegInt(p, return_errors = false) {
     errors = []; // assume no errors at first
-    if(p == '') p =0; // handle blank inputs as if they are 0
-    if (Number(p) != p) errors.push('<font color="red">Not a number!</font>'); // Check if string is a number value
-    else if (p < 0) errors.push('<font color="red">Negative value!</font>'); // Check if it is non-negative
-    else if (parseInt(p) != p) errors.push('<font color="red">Not an integer!</font>'); // Check that it is an integer
+    if (p == '') p = 0; // handle blank inputs as if they are 0
+    if (Number(p) != p) errors.push(''); // Check if string is a number value
+    else if (p < 0) errors.push(''); // Check if it is non-negative
+    else if (parseInt(p) != p) errors.push('<'); // Check that it is an integer
     return return_errors ? errors : (errors.length == 0);
 }
 
