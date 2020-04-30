9727f408e5b198d12675e32bc041dcafca1a3c87
mikeylangelo <60157511+mikeylangelo@users.noreply.github.com>
2020-04-30 11:19:35 -1000

    assignment2

--- Ferrer_Michelangelo_Assignment2/server.js
+++ Ferrer_Michelangelo_Assignment2/server.js
@@ -9,20 +9,18 @@ var product_data = require('./public/info/product_data.js');
 var products = product_data.products;
 
 //Assign JSON files to a variable
-var pdata = './public/info/purchasedata.json';
 var udata = './public/info/userdata.json';
 
 //Get raw data files
-var unparsedpurchasedata = fs.readFileSync(pdata, 'utf-8');
 var unparseduserdata = fs.readFileSync(udata, 'utf-8');
 
 //Parse the raw data as JSON files
 var userdata = JSON.parse(unparseduserdata);
-var purchasedata = JSON.parse(unparsedpurchasedata);
 
 //Read the templates and assign to variable
 var invoice_contents = fs.readFileSync('./public/invoice.html', 'utf8');
 var display_contents = fs.readFileSync('./public/display.html', 'utf8');
+var error_contents = fs.readFileSync('./public/error.html', 'utf8');
 
 //Indicate any requests coming in from the server
 app.all('*', function (request, response, next) {
@@ -43,35 +41,21 @@ app.get("/display.html", function (request, response) {
 });
 
 app.post("/display.html", function (request, response, ) { //Handles all POST requests
-    let POST = request.body; //Request the POST data
+    let POST = request.body; //Request the POST data from body
 
     for (i = 0; i < products.length; i++) {
-        p = POST["quantity${i}"]
-        if (typeof POST['purchase_flowers'] != 'undefined') {
-            p = POST["quantity${i}"]
-            console.log(request.body.p)
-            //Passes data through isNonNegInt() if it is false it create the invoice else it will redirect to an error page
-            if (isNonNegInt(p, true)) {
-                response.send(eval('`' + invoice_contents + '`')); //Evaluates the invoice.html file and passes the invoice which contains a function
-                input_data = request.body["quantity"];
-                purchasedata[input_data] = {};
-                purchasedata[input_data].quantity = request.body.quantity
-                fs.writeFileSync(data, JSON.stringify(purchasedata));
+        p = POST[`quantity${i}`]
+        console.log(p)
 
-                /* request.body.p;
-                 purchasedata[p] = {};
-                 purchasedata[p].quantity = request.body
-                 fs.writeFileSync(pdata, JSON.stringify(p))
-                 */
-                //Redirect to login before generating invoice
-                //response.redirect('login.html')
-            }
-            else if (request.body.quantity = '') {
-                console.log('Blank display') //Error if there is blank data
-            }
-            else {
-                console.log('invalid data'); //If it does not pass redirect to an error page.
-            }
+        //Passes data through isNonNegInt() if it is false it create the invoice else it will redirect to an error page
+        if ((p<0) || (Number(p) != p)){
+            err_str = 'incorrect data'
+            response.redirect(`error.html?error=${err_str}`); //If it does not pass redirect to an error page.    
+        }
+
+        //Quantities are okay. Redirect to login
+        else {
+            response.redirect(`login.html?quantity=p`);
         }
 
     }
@@ -94,18 +78,60 @@ app.post("/login.html", function (request, response) {
     // Succesful login. Match password with username and generate invoice
     if (user_info["password"] == input_password) {
             console.log('succesful login');
+            let POST = request.query
+            console.log(request.query)
             response.send(eval('`' + invoice_contents + '`'));
+            function display_invoice_table_rows() {
+                subtotal = 0;
+                str = '';
+                for (i = 0; i < products.length; i++) {
+                    p = POST[`quantity${i}`]; //Defines any POST requests which have been defined in display.html
+                    if (typeof POST[`quantity${i}`] != 'undefined') {
+                        console.log('No data');
+                    }
+                    /* Based on order_page.html of Lab 12 completed by me*/
+                    if (p > 0) {
+                        extended_price = p * products[i].price
+                        subtotal += extended_price;
+                        str += (`
+                            <tr>
+                                <td width="43%">${products[i].name}</td>
+                                <td align="center" width="11%">${p}</td>
+                                <td width="13%">\$${products[i].price.toFixed(2)}</td>
+                                <td width="54%">\$${extended_price.toFixed(2)}</td>
+                            </tr>
+                            `);
+                    }
+                }
+            
+                // Compute tax
+                tax_rate = 0.1;
+                tax = tax_rate * subtotal;
+            
+                // Compute grand total
+                grandtotal = subtotal + tax;
+            
+                return str;
+            }
         }
+        
+        
 
     // If the password and username do not match redirect to error
     else {
         console.log('password and username dont match');
+        err_str = 'password and username dont match';
+        response.redirect(`./error.html?error=${err_str}`)
         }
     
     // If the username does not exist
     }
     else {
         console.log('invalid login data');
+    }
+
+    function display_error(){
+        request.query["error"];
     }
 });
 
@@ -139,6 +165,41 @@ app.post("/register.html", function (request, response) {
     else {
         console.log('success register');
         fs.writeFileSync(udata, JSON.stringify(userdata));
+        let POST = request.query
+        console.log(request.query)
+        response.send(eval('`' + invoice_contents + '`'));
+            function display_invoice_table_rows() {
+                subtotal = 0;
+                str = '';
+                for (i = 0; i < products.length; i++) {
+                    p = POST[`quantity${i}`]; //Defines any POST requests which have been defined in display.html
+                    if (typeof POST[`quantity${i}`] != 'undefined') {
+                        console.log('No data');
+                    }
+                    /* Based on order_page.html of Lab 12 completed by me*/
+                    if (p > 0) {
+                        extended_price = p * products[i].price
+                        subtotal += extended_price;
+                        str += (`
+                            <tr>
+                                <td width="43%">${products[i].name}</td>
+                                <td align="center" width="11%">${p}</td>
+                                <td width="13%">\$${products[i].price.toFixed(2)}</td>
+                                <td width="54%">\$${extended_price.toFixed(2)}</td>
+                            </tr>
+                            `);
+                    }
+                }
+            
+                // Compute tax
+                tax_rate = 0.1;
+                tax = tax_rate * subtotal;
+            
+                // Compute grand total
+                grandtotal = subtotal + tax;
+            
+                return str;
+            }
     }
 });
 
@@ -164,8 +225,7 @@ function display_products() {
          $${products[i].price.toFixed(2)}
          </div>
          <div>
-         <input type="text" name="quantity${i}" 
-         onkeyup="checkQuantityTextbox(this);">
+         <input type="text" name="quantity${i}">
          </div
      </section>
         `;
@@ -178,8 +238,8 @@ function display_invoice_table_rows() {
     subtotal = 0;
     str = '';
     for (i = 0; i < products.length; i++) {
-        p = POST[`${products[i].name}`]; //Defines any POST requests which have been defined in display.html
-        if (typeof POST[`${products[i].name}`] != 'undefined') {
+        p = POST["quantity{i}"]; //Defines any POST requests which have been defined in display.html
+        if (typeof POST["quantity{i}"] != 'undefined') {
             console.log('No data');
         }
         /* Based on order_page.html of Lab 12 completed by me*/
@@ -206,20 +266,4 @@ function display_invoice_table_rows() {
     grandtotal = subtotal + tax;
 
     return str;
-}
-
-function isNonNegInt(p, return_errors = false) {
-    errors = []; //Initially assume no errors
-    if (p == '') p = 0; //Blank inputs is zero
-    if (Number(p) != p) errors.push(''); //Check if string is a number value
-    else if (p < 0) errors.push(''); //Check if it is non-negative
-    else if (parseInt(p) != p) errors.push('<'); //Check that it is an integer
-    return return_errors ? errors : (errors.length == 0);
-}
-
-function checkQuantityTextbox(theTextbox) {
-    errs = isNonNegInt(theTextbox.value, true);
-    if (errs.length == 0) errs = ['You want:'];
-    if (theTextbox.value.trim() == '') errs = ['Quantity'];
-    document.getElementById(theTextbox.name + '_label').innerHTML = errs.join(", ");
 }
