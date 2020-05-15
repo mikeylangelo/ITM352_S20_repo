966f4122ededce3e04ec696bead3aa85339fcd9d
mikeylangelo <60157511+mikeylangelo@users.noreply.github.com>
2020-05-15 13:41:05 -1000

    5/15

--- Ferrer_Michelangelo_Assignment3/server.js
+++ Ferrer_Michelangelo_Assignment3/server.js
@@ -9,6 +9,24 @@ app.use(bodyParser());
 var session = require('express-session');
 app.use(session({ secret: "ITM 352" }));
 
+var nodemailer = require('nodemailer'); //Loads the nodemailer module
+
+var transporter = nodemailer.createTransport({ //Determines who is the transporter of the email. I just used dummy email
+    service: 'hawaii',
+    auth: {
+      user: 'my@hawaii.edu', 
+      pass: 'mypassword'
+    }
+  });
+
+  var mailOptions = { //Determines the content of the email
+    from: 'my@hawaii.edu',
+    to: `${session.email}`,
+    subject: 'Thank You!',
+    text: `Thank you for your purchase ${session.username}!
+    -Local Florist`
+  };
+
 var fs = require('fs'); //Load file system
 var qs = require('querystring'); //Load query string module
 
@@ -22,12 +40,7 @@ var unparseduserdata = fs.readFileSync(udata, 'utf-8');
 var userdata = JSON.parse(unparseduserdata);
 
 //Read the templates and assign to variable. Also require data from the public file. These will be used in our first five app.get
-
-
-
-
 var cart_contents = fs.readFileSync('./public/cart.html', 'utf8')
-
 var invoice_contents = fs.readFileSync('./public/invoice.html', 'utf8');
 
 //Variable for all input data on display
@@ -39,6 +52,7 @@ app.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path);
     next();
 });
+
 
 /* 
 The structure for the display and invoice generationwas based on the MVC Assignment 1 example from Professor Port.
@@ -58,7 +72,6 @@ Their are different arrays for every type of option in the product_data.js file.
 app.get("/bouquet.html", function (request, response) {
     var display_bouquet_contents = fs.readFileSync('./public/bouquet.html', 'utf8');
     var bouquet_data = require('./public/info/bouquet_data.js');
-
     var products = bouquet_data.products;
     response.send(eval('`' + display_bouquet_contents + '`'));
 });
@@ -71,7 +84,6 @@ app.get("/seed.html", function (request, response) {
 });
 
 app.get("/soil.html", function (request, response) {
-
     var display_soil_contents = fs.readFileSync('./public/soil.html', 'utf8');
     var soil_data = require('./public/info/soil_data.js');
     var products = soil_data.products;
@@ -118,9 +130,9 @@ app.post("/addToCart", function (request, response, ) { //Handles all POST reque
         var q_str = qs.stringify(errs_array);
         response.redirect(`error.html?${q_str}`);
     }
-    //Quantities are okay. Redirect to login
+    //Quantities are okay
     else {
-        response.redirect('index.html');
+        response.redirect('bouquet.html');
     }
 
 });
@@ -133,7 +145,11 @@ app.post("/purchaseCart", function (request, response) {
     let POST = request.body;
     cart_quantities = POST;
     q = POST["cartquantity"];
-    response.redirect('login.html');
+    if (session.username != undefined) {
+        response.send(eval('`' + invoice_contents + '`'));
+    } else {
+        response.redirect('login.html');
+    }
 })
 
 /* 
@@ -159,6 +175,9 @@ app.post("/login.html", function (request, response) {
             session.username = input_username; //If login succesful assign last login time and username to the session.
             session.last_login = theDate;
             response.send(eval('`' + invoice_contents + '`'));
+            transporter.sendMail(mailOptions, function(info){
+                info.response
+              });
         }
 
         // If the password and username do not match redirect to error
@@ -208,9 +227,17 @@ app.post("/register.html", function (request, response) {
         userdata[request.body.username].password = request.body.password;
         userdata[request.body.username].repeatpassword = request.body.repeat_password;
         userdata[request.body.username].email = request.body.email;
+
         fs.writeFileSync(udata, JSON.stringify(userdata));
+        
         session.username = request.body.username; //If register succesful save username to session.
+        session.email = request.body.email;
+
         response.send(eval('`' + invoice_contents + '`'));
+
+        transporter.sendMail(mailOptions, function(info){
+            info.response
+          });
     }
 });
 
@@ -222,6 +249,7 @@ app.listen(8080, () => console.log(`Listening...`));
 //Used in response.send(eval(contents)) app.get(store.html). Generates display page
 function display_products() {
     str = '';
+    str += `<input type='submit' id="addToCart" value="Add To Cart">`
     for (i = 0; i < products.length; i++) {
         str += `
     <section>
@@ -236,7 +264,6 @@ function display_products() {
          </div>
          <div>
          <input placeholder=0 type="text" name="quantity${i}">
-         <input type='submit' id="addToCart" value="Add To Cart">
          <br>
          </div
          
@@ -254,7 +281,8 @@ function display_shopping_cart() {
     if (`${session.username}` != 'undefined') {
         str += `<p>${session.username}'s Shopping Cart</p>`
     } else {
-        str += `<p>Your Shopping Cart</p>`
+        str += `<p>Your Shopping Cart</p>
+        <input id="purchaseCart" type="submit" value="Continue to Checkout">`
     }
     for (i = 0; i < products.length; i++) {
         POST = store_quantities; //Gets the POST requests stored from store.html
@@ -269,7 +297,7 @@ function display_shopping_cart() {
                     <td width="13%">\$${products[i].price.toFixed(2)}</td>
                     <td align="center" width="11%"><input type="number" min="1" name="cartquantity${i}" value="${p}"></td>
                 </tr>
-                <input id="purchaseCart" type="submit" value="Purchase Cart">
+                
                 `);
         }
     }
@@ -303,8 +331,20 @@ function display_invoice_table_rows() {
     tax_rate = 0.1;
     tax = tax_rate * subtotal;
 
+    // Compute Shipping
+    if (subtotal >= 50) {
+        shipping = 5.00
+    }
+    else if (subtotal >= 25) {
+        shipping = 2.50
+    }
+    else {
+        shipping = .05 * subtotal
+    }
+
     // Compute grand total
-    grandtotal = subtotal + tax;
+    grandtotal = subtotal + tax + shipping;
+
     return str;
 }
 
